f7707e0d2c06930fdfa8c62431bfe61099ec08a6
mouddibi <baradine-abakar.mahamat@etudiant.univ-rennes.fr>
2024-12-11 16:10:00 +0100

    message test_chargement de test_terrain

--- test/test_terrain.py
+++ test/test_terrain.py
@@ -7,8 +7,26 @@ from Terrain import Terrain, Case
 class TestTerrain(unittest.TestCase):
 
     def test_chargement(self):
-        # TODO
-        self.fail()
+        t= Terrain()
+        fichier_test='t1.txt'
+        t.charger(fichier_test)
+        
+        self.assertEqual(t.largeur,21)
+        self.assertEqual(t.hauteur,10)
+        
+        
+        self.assertEqual(t[9][17],Case.ENTREE)
+        self.assertEqual(t[2][8], Case.CLIENT)
+        self.assertEqual(t[4][15],Case.CLIENT)
+        self.assertEqual(t[7][6], Case.CLIENT)
+        
+        
+        self.assertEqual(t[0][0], Case.VIDE)
+        self.assertEqual(t[6][12],Case.VIDE)
+        
+        
+        self.assertEqual(t[4][10], Case.OBSTACLE)
+        
 
     def test_accesseur(self):
         t = Terrain()
